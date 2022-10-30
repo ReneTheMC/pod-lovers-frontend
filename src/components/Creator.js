5e9a8b1a58fa80687cac3308e0d99c0ca1852e11
hninmabalo <hninmabalo@gmail.com>
2022-10-29 22:22:29 -0700

    chore: create PodcastList.js and Creator.js

---
+++ src/components/Creator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Creator = () => {
+    return (
+        <div>
+            <h1>Our famous creator</h1>
+        
+        </div>
+    )
+}
+
+export default Creator;
---
+++ src/components/PodcastList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PodcastList = () => {
+    return (
+        <div>
+            <h1>Popular Podcasts</h1>
+        
+        </div>
+    )
+}
+
+export default PodcastList;
