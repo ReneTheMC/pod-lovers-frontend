729cd6935986d1e3f8465672423c0b24474e0d2d
hninmabalo <hninmabalo@gmail.com>
2022-11-09 13:04:59 -0800

    update css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Podcast from './Podcast';
 
 const Welcome = () => {
     return (
         <div>
-            <Podcast />
+            <h2>There are a few different methods for achieving peace of mind. Have you try listen Podcast? If not, start listen for popular podcast.</h2>
+            <hr />
+            <p>Start searching for popular podcast
+                <form action="/podcast" className="form-inline">
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </p>
+            <p>Start searching for famous creator 
+                <form action="/creator" className="form-inline">
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form></p>
+
         </div>
     )
 }
