f48590d691316c54f60ff7c5fdcc2e27b2b9e5f9
hninmabalo <hninmabalo@gmail.com>
2022-11-09 17:49:41 -0800

    update: readme and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer bg-dark">
             <div className="container text-center">
-                <span className="text-muted">@2022</span>
+                <span className="text-muted">Created by <a href="/about">PodLovers Team</a> @2022</span>
             </div>
         </footer>
     )
